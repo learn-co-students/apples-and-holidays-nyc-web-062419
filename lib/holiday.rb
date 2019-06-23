9ad76276df2c7f6c011c2431e5af5e70527ba83d
ghatzid <ghatzid@gmail.com>
2019-06-23 15:56:12 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,17 +61,25 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+
+ 
   holiday_hash.each do |season, holiday|
-      puts "#{season.capitalize}:"
-      season.each do |holiday, supplies|
+    puts "#{season.capitalize}:"
+      holiday.each do |holiday, supplies|
+        puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
       end
   end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  def all_holidays_with_bbq(supplies_hash)
+    supplies_hash.map do |season, holidays|
+      holidays.map do |holiday, supplies|
+        holiday if supplies.include?("BBQ")
+      end
+    end.flatten.compact
+  end
 end
 
 
